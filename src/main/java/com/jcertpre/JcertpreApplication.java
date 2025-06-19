9379d0c6a405ed94a8fa50003ca65dce0559c965
khuongle247 <khuong0352391308@gmail.com>
2025-06-19 15:50:27 +0700

    Initial commit - Spring Boot JCertPre project

---
+++ src/main/java/com/jcertpre/JcertpreApplication.java
@@ -0,0 +1,13 @@
+package com.jcertpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcertpreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcertpreApplication.class, args);
+	}
+
+}
